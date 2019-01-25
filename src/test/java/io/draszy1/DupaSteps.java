afe4fdc0a641b4eab31608ed91d7a7de9c06fb84
Szymon Drawski <s.drawski@carfax.eu>
2019-01-25 09:55:40 +0100

    selenium works

--- src/test/java/io/draszy1/DupaSteps.java
+++ src/test/java/io/draszy1/DupaSteps.java
@@ -1,32 +1,49 @@
 package io.draszy1;
 
-import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DupaSteps {
+
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+        this.driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        this.driver.quit();
+    }
+
     @Given("^Google page$")
     public void the_Symbiote_home_page() throws Throwable {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://google.pl");
-        assertEquals("Google", driver.getTitle());
-        driver.quit();
+        assertThat(driver.getTitle()).isEqualTo("Google");
     }
 
     @When("^I search for dupa$")
-    public void logging_in_as_an_admin() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void search_for_dupa() throws Throwable {
+        WebElement searchInput = driver.findElement(By.cssSelector("input[title='Search']"));
+        searchInput.sendKeys("dupa");
+
+        WebElement button = driver.findElement(By.name("btnK"));
+        button.submit();
     }
 
-    @Then("^Wikipedia about dupa is found$")
+    @Then("^Wikipedia about dupa is found on the first place$")
     public void the_home_page_navigation_is_available() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        WebElement output = driver.findElements(By.cssSelector("cite[class='iUh30']")).get(0);
+        assertThat(output.getText()).contains("wiki");
     }
 }
--- src/test/resources/dupa.feature
+++ src/test/resources/dupa.feature
@@ -3,4 +3,4 @@ Feature: Dupa
   Scenario: Find dupa in Google
     Given Google page
     When I search for dupa
-    Then Wikipedia about dupa is found
+    Then Wikipedia about dupa is found on the first place
